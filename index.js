fb6d1ee0433d42edcd099f3d4d21c9368eae4caf
redhoyasa <redhoyasa@gmail.com>
2023-03-27 10:41:08 +0700

    add ping API

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
